adcd95216ad6be2a8f34aa7f1608ee200b9991d4
Scott Neel <scott@neel.net>
2024-02-01 15:33:58 -0800

    more devcontainer fixes

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -4,10 +4,6 @@ set -euxo pipefail
 
 pip install --disable-pip-version-check --upgrade pip
 
-pip install pre-commit
-
 pip install -r requirements_dev.txt &
-
-pre-commit install --install-hooks &
 
 wait
